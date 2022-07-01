da1b4cc41954067fab0e3ad0d2920db6b2a8f36b
Maikon Alexandre <maikonalexandre574@gmail.com.br>
2022-06-30 22:56:37 -0300

    first commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+
+export default function Dashboard(){
+    return (
+        <Flex>
+        <Header/>
+
+        <Flex w='100%' my='6' maxW={1400} mx='auto' px='6'>
+        
+        </Flex>
+        </Flex>
+
+    )
+}
